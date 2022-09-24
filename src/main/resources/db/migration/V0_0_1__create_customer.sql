11b199abf6827bfa6cbe51e4f6ac5b329cab6707
mertosi <mertkondakcioglu@gmail.com>
2022-09-24 09:20:37 +0300

    flyway sql file reformatted.

--- src/main/resources/db/migration/V0_0_1__create_customer.sql
+++ src/main/resources/db/migration/V0_0_1__create_customer.sql
@@ -1,37 +1,13 @@
 create table if not exists tbl_customer
 (
-    id
-    bigint
-    unsigned
-    not
-    null
-    auto_increment,
-    email
-    varchar
-(
-    100
-) not null,
-    password varchar
-(
-    100
-) not null,
-    first_name varchar
-(
-    100
-) not null,
-    last_name varchar
-(
-    100
-) not null,
-    created_at timestamp null default current_timestamp,
-    updated_at timestamp null default null on update current_timestamp,
-    primary key
-(
-    id
-),
-    unique key uq_customer
-(
-    email
-)
-    ) engine = innodb;
+    id                  bigint unsigned not null auto_increment,
+    email               varchar(100) not null,
+    password            varchar(100) not null,
+    first_name          varchar(100) not null,
+    last_name           varchar(100) not null,
+    created_at          timestamp null default current_timestamp,
+    updated_at          timestamp null default null on update current_timestamp,
+    primary key (id),
+    unique key uq_customer (email)
+) engine = innodb;
 
