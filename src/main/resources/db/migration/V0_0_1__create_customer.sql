7d94d0b5b9ca821e25e0f2ab983d5bc3e4cfc81c
mertosi <mertkondakcioglu@gmail.com>
2022-09-30 13:00:22 +0300

    tbl_api_log created with flyway sql file.

--- src/main/resources/db/migration/V0_0_1__create_customer.sql
+++ src/main/resources/db/migration/V0_0_1__create_customer.sql
@@ -11,3 +11,17 @@ create table if not exists tbl_customer
     unique key uq_customer (email)
 ) engine = innodb;
 
+create table if not exists tbl_api_log
+(
+    id                  bigint unsigned not null auto_increment,
+    user                varchar(100) not null,
+    user_agent          varchar(255) default null,
+    uri                 varchar(255) not null,
+    ip                  varchar(255) not null,
+    request             longtext,
+    response            longtext not null,
+    created_at          timestamp null default current_timestamp,
+    created_by          varchar(100) default 'system',
+    primary key (id)
+) engine = innodb;
+
